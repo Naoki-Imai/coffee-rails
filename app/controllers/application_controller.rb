bb37cbaa2411736cde88466dd0e950b7ba474cc7
Naoki Imai <naokinetin0110@gmail.com>
2021-01-30 21:51:13 +0900

    devise user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
+  protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image])
+  end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'coffees' => 'coffees#index'
+  get 'coffees/edit'
+  get 'coffees/new'
+  get 'coffees/show'
+  get 'users/index' => 'users#index'
+  get "users/show/:name" => 'users#show'
+  devise_for :users
   root 'homes#top'
 end
